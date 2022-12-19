ef2d41d707b935861a7230fd8eaaedbd9410b546
sowjanya-madhupada <sowjimadhupada@gmail.com>
2022-12-19 16:42:40 +0530

    second commit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,6 @@
 class AdminController < ApplicationController
-  before_action :check_log
+  before_action :check_admin
   def home
   end
 
-  def check_log
-    binding.pry
-    return if admin_signed_in?
-
-    cookies[:return_to] = request.url
-    redirect_to new_admin_session_path, gflash: { notice: "You must be logged in to do that" }
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
     
 	def check_admin
-	    return if current_user.admin?
+	    return if current_user && current_user.admin?
 	    redirect_to(root_path, flash: { alert: "Sorry, you are not allowed to view that page" })
     end
+
+    def current_user
+	    return unless session[:user_id]
+
+	    @current_user ||= User.find_by(id: session[:user_id])
+	end
 	
 	def after_sign_in_path_for(user)
 		flash[:notice]="Login successful. Hi, #{current_user.name}!" 
@@ -19,7 +24,7 @@ class ApplicationController < ActionController::Base
     protected
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :phone_number, :password) }
-            #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :phone_number, :password, :current_password) }
         end
 	
 end
