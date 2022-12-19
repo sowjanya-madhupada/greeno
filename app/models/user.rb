9b7eebb4860e6c3d19d852e1db94c5f9e4ad3b24
sowjanya-madhupada <sowjimadhupada@gmail.com>
2022-12-19 18:25:36 +0530

    added validations to user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :phone_number, presence: true, numericality: true, length: { minimum: 7, maximum: 15 }
 end
