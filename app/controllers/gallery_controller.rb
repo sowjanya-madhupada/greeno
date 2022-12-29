2945905377efc07bbc2a6375c398765f150d1ba6
sowjanya-madhupada <sowjimadhupada@gmail.com>
2022-12-29 20:22:38 +0530

    search and form styling changes

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,10 +1,13 @@
 class GalleryController < ApplicationController
   def index
     @plants = Store.all
-  end
-
-  def search
-    keyword = "%"+params[:search].to_s+"%"
-    @plants = Store.find_by_sql ["Select * from stores WHERE name like? or season like? or plants_type like?",keyword,keyword,keyword]
+    return @plants if params[:q].blank? && params[:filter]=="All"
+    @plants = @plants.where("name like? ","%"+params[:q].to_s+"%") if params[:q]
+    filter_array = ["Flowers","Indoor","Outdoor","Gifts"]
+    if filter_array.any?(params[:filter])
+      @plants = @plants.where("plants_type like? ","%"+params[:filter].to_s+"%")
+    elsif !(params[:filter]=="All")
+      @plants = @plants.where("season like? ","%"+params[:filter].to_s+"%")
+    end 
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def filter_by_plant
+		['All', 'Flowers', 'Indoor', 'Outdoor', 'Gifts', 'Summer', 'Winter', 'Spring', 'Any Season' ]
+	end
 end
