a0708325aa623cf81c89311d00818694606926c9
sowjanya-madhupada <sowjimadhupada@gmail.com>
2023-01-03 20:25:54 +0530

    removed string conversion

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,12 +2,14 @@ class GalleryController < ApplicationController
   def index
     @plants = Store.where.not(quantity: 0)
     return @plants if params[:q].blank? && params[:filter]=="All"
-    @plants = @plants.where("name like? ","%"+params[:q].to_s+"%") if params[:q]
-    filter_array = ["Flowers","Indoor","Outdoor","Gifts"]
-    if filter_array.any?(params[:filter])
-      @plants = @plants.where("plants_type like? ","%"+params[:filter].to_s+"%")
-    elsif !(params[:filter]=="All")
-      @plants = @plants.where("season like? ","%"+params[:filter].to_s+"%")
-    end 
+    @plants = @plants.where("name like? ","%"+params[:q]+"%") if params[:q]
+    if params[:filter]
+      filter_array = ["Flowers","Indoor","Outdoor","Gifts"]
+      if filter_array.any?(params[:filter])
+        @plants = @plants.where("plants_type like? ","%"+params[:filter]+"%")
+      elsif !(params[:filter]=="All")
+        @plants = @plants.where("season like? ","%"+params[:filter]+"%")
+      end 
+    end
   end
 end
