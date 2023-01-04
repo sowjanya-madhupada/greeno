1ad56bb5c2da0374d957e5c8818f377783baeb70
sowjanya-madhupada <sowjimadhupada@gmail.com>
2023-01-04 10:18:45 +0530

    search query improvement

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,15 +1,13 @@
 class GalleryController < ApplicationController
   def index
     @plants = Store.where.not(quantity: 0)
-    return @plants if params[:q].blank? && params[:filter]=="All"
+    return @plants unless params[:q] && params[:filter]
     @plants = @plants.where("name like? ","%"+params[:q]+"%") if params[:q]
-    if params[:filter]
-      filter_array = ["Flowers","Indoor","Outdoor","Gifts"]
-      if filter_array.any?(params[:filter])
-        @plants = @plants.where("plants_type like? ","%"+params[:filter]+"%")
-      elsif !(params[:filter]=="All")
-        @plants = @plants.where("season like? ","%"+params[:filter]+"%")
-      end 
-    end
+    filter_array = ["Flowers","Indoor","Outdoor","Gifts"]
+    if filter_array.any?(params[:filter])
+      @plants = @plants.where("plants_type like? ","%"+params[:filter]+"%")
+    elsif !(params[:filter]=="All")
+      @plants = @plants.where("season like? ","%"+params[:filter]+"%")
+    end 
   end
 end
